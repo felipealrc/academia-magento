67c0ecdaeaa00091dcc9e67806024fd592248fd6
LUIS FELIPE MARTINS ALARCON <luis.alarcon@buscapecompany.com.br>
2018-10-24 19:59:46 -0300

    olar mundo

---
+++ app/code/local/Academia/HelloWorld/controllers/IndexController.php
@@ -0,0 +1,11 @@
+<?php
+class Academia_HelloWorld_IndexController extends Mage_Core_Controller_Front_Action{
+    
+    public function indexAction(){
+        echo "hello World";
+    }
+
+    public function oiAction(){
+        echo "Olar";
+    }
+}
---
+++ app/code/local/FCamara/OlaMundo/controllers/IndexController.php
@@ -0,0 +1,8 @@
+<?php
+class FCamara_OlaMundo_IndexController extends Mage_Core_Controller_Front_Action{
+    
+    public function indexAction()
+    {
+        echo "olar";
+    }
+}
